63f341f88ed3fbd1d00f0be348359b4b633a9bc7
Yazhong Liu <yorkiefixer@gmail.com>
2014-06-06 19:21:03 +0800

    opt

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ exports.build = buildX509;
 
 function buildX509(buffer) {
   if (typeof buffer !== 'string') {
-  	buffer = buffer.toString('base64');
+    buffer = buffer.toString('base64');
   }
   var result = '-----BEGIN CERTIFICATE-----\n';
   var offset = 0;
-  while (offset < buffer.length) {
+  var len = buffer.length;
+  while (offset < len) {
     result += buffer.slice(offset, offset + 64) + '\n';
     offset += 64;
   }
